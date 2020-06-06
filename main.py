0499bebcb49d2d6f9f6420e3fc58b075c569b13a
Amirreza Amouie <aamouie01@gmail.com>
2020-06-06 01:33:56 -0700

    add grammars, fix bugs, ask for grammar number input

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ from util import *
 rules = {}
 expressions = []
 stack = []
+grammar_no = 0
 
-def read_grammars():
+def read_grammars(grammar_no):
     with open('input.json') as input_file:
         data = json.load(input_file)
-
         # fill the rules list
         for g in data['grammars']:
-            for i in range(0, len(data['grammars'][g])): 
-                new_rule=split_rule(data['grammars'][g][i])
-                if new_rule[0] not in rules:
-                    rules[new_rule[0]]=[new_rule[1]]
-                else:
-                    rules[new_rule[0]].append(new_rule[1])
+            if g == grammar_no:
+                for i in range(0, len(data['grammars'][g])): 
+                    new_rule=split_rule(data['grammars'][g][i])
+                    if new_rule[0] not in rules:
+                        rules[new_rule[0]]=[new_rule[1]]
+                    else:
+                        rules[new_rule[0]].append(new_rule[1])
 
-def get_expression_input():
+def input_expression():
     input_chars = input("Enter the expression: ").split(" ")
     for char in input_chars:
         expressions.append(char)
+
+def input_grammar_number():
+    return input("Which grammar would you like to check your expression with (from 1-5): ")
+    
     
 def check_expression(stack, expression):
     
@@ -59,8 +64,9 @@ def check_expression(stack, expression):
         
         check_expression(tmp_stack, copy.deepcopy(expression))
 
-read_grammars()
-get_expression_input()
+grammar_no = input_grammar_number()
+read_grammars(grammar_no)
+input_expression()
 stack.append(next(iter(rules)))
 
 print("\n#####################\n")
@@ -70,6 +76,3 @@ print("\n#####################\n")
 
 if not check_expression(stack, expressions):
     print("FAIL")
-
-
-
--- util.py
+++ util.py
@@ -2,4 +2,6 @@ def split_rule(input):
     tmp = input.split(' -> ')
     variable = tmp[0]
     production = tmp[1]    
+    print(variable)
+    print(production)
     return [variable, production]
